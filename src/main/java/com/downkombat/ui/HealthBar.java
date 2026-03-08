6d9a91e43b8bfd066b41e655d135bc9fd030a4f2
Irene Pascual Moreno <irenepascualmoreno@gmail.com>
2026-03-08 15:05:42 +0100

    feat: improve health bar with dynamic scaling and color states

--- src/main/java/com/downkombat/ui/HealthBar.java
+++ src/main/java/com/downkombat/ui/HealthBar.java
@@ -1,34 +1,33 @@
 package com.downkombat.ui;
 
+import com.downkombat.config.GameConfig;
 import javafx.scene.Group;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class HealthBar {
 
-    private Rectangle background;
-    private Rectangle bar;
-
     private static final double WIDTH = 400;
     private static final double HEIGHT = 30;
 
-    public HealthBar(double x, double y, Color color) {
+    private Rectangle background;
+    private Rectangle bar;
+
+    private Group container;
+
+    public HealthBar(double x, double y, Color baseColor) {
 
         background = new Rectangle(WIDTH, HEIGHT);
         background.setFill(Color.DARKGRAY);
 
         bar = new Rectangle(WIDTH, HEIGHT);
-        bar.setFill(color);
+        bar.setFill(baseColor);
 
-        Group group = new Group(background, bar);
+        container = new Group(background, bar);
 
-        group.setTranslateX(x);
-        group.setTranslateY(y);
-
-        container = group;
+        container.setTranslateX(x);
+        container.setTranslateY(y);
     }
-
-    private Group container;
 
     public Group getNode() {
         return container;
@@ -36,8 +35,29 @@ public class HealthBar {
 
     public void update(int health) {
 
-        double percentage = health / 100.0;
+        double percentage = (double) health / GameConfig.PLAYER_MAX_HEALTH;
+
+        // protección por seguridad
+        percentage = Math.max(0, Math.min(percentage, 1));
 
         bar.setWidth(WIDTH * percentage);
+
+        updateColor(percentage);
+    }
+
+    private void updateColor(double percentage) {
+
+        if (percentage > 0.5) {
+
+            bar.setFill(Color.LIMEGREEN);
+
+        } else if (percentage > 0.25) {
+
+            bar.setFill(Color.GOLD);
+
+        } else {
+
+            bar.setFill(Color.RED);
+        }
     }
 }
